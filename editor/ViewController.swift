a9bddbc7e787dad12c7ab6e9526df323a72c4806
max <yd0095@gmail.com>
2020-02-12 13:20:20 +0900

    window추가, debug용 list와 delegate table method추가

--- editor/ViewController.swift
+++ editor/ViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var name = ["a","b","c"]
+    var image = ["1.jpeg","2.jpeg","3.jpeg"]
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
         
     }
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableCell
+        cell.cellImage.image = UIImage(named: image[indexPath.row])
+        cell.title.text = name[indexPath.row]
+        cell.contents.text = name[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
