de0d00d42ea11d8f1ab68f22cc477a063d5ddb3f
max <yd0095@gmail.com>
2020-02-12 17:12:19 +0900

    back기능 추가

--- editor/TextViewController.swift
+++ editor/TextViewController.swift
@@ -9,6 +9,31 @@
 import UIKit
 
 class TextViewController : UIViewController {
+    @IBAction func backBtn(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "title", message: "저장하시겠습니까?", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default){
+                        _ in
+            //TODO 확인 시 저장할것..
+                        self.performSegue(withIdentifier: "ToList", sender: self)
+                   }
+        let cancelAction = UIAlertAction(title:"취소",style: .cancel){
+            _ in
+            self.performSegue(withIdentifier: "ToList", sender: self)
+        }
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        alertController.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
+        //TODO 저장기능
+    }
+    
+    
+    
+    
+    
+    
     
     
     
