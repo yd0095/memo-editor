83ffb45d353854d866e0181ee0211056bab3bac3
max <yd0095@gmail.com>
2020-02-13 14:12:14 +0900

    back 기능 보완

--- editor/TextViewController.swift
+++ editor/TextViewController.swift
@@ -9,28 +9,30 @@
 import UIKit
 
 class TextViewController : UIViewController {
+
     @IBAction func backBtn(_ sender: UIBarButtonItem) {
-        let alertController = UIAlertController(title: "title", message: "저장하시겠습니까?", preferredStyle: .alert)
-        let confirmAction = UIAlertAction(title: "확인", style: .default){
-                        _ in
-            //TODO 확인 시 저장할것..
-                        self.performSegue(withIdentifier: "ToList", sender: self)
-                   }
-        let cancelAction = UIAlertAction(title:"취소",style: .cancel){
-            _ in
-            self.performSegue(withIdentifier: "ToList", sender: self)
-        }
-        alertController.addAction(confirmAction)
-        alertController.addAction(cancelAction)
-        alertController.present(alertController, animated: true, completion: nil)
+     let alertController = UIAlertController(title: "title", message: "저장하시겠습니까?", preferredStyle: .alert)
+     let confirmAction = UIAlertAction(title: "확인", style: .default){
+                     _ in
+         //TODO 확인 시 저장할것..
+            self.navigationController?.popViewController(animated: true)
+                }
+     let cancelAction = UIAlertAction(title:"취소",style: .cancel){
+         _ in
+            self.navigationController?.popViewController(animated: true)
+     }
+     alertController.addAction(confirmAction)
+     alertController.addAction(cancelAction)
+     self.present(alertController, animated: true, completion: nil)
+        
     }
+    
+    
     
     @IBAction func saveBtn(_ sender: UIBarButtonItem) {
         //TODO 저장기능
     }
-    
-    
-    
+
     
     
     
